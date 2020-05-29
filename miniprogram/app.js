362ec173322266c5aabb394265a5b039751ecfda
cong <pengyuancong@ib.com.cn>
2020-05-29 15:46:55 +0800

    小程序云开发、数据库记录

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,16 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function (opts) {
+
+    console.log('App Launch', opts)
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true
+      })
+    }
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
--- miniprogram/page/sleep/index.js
+++ miniprogram/page/sleep/index.js
@@ -1,6 +1,7 @@
 // page/sleep/index.js
 const dateFormat = require('../../miniprogram_npm/miniprogram-licia/dateFormat');
 const random = require('../../miniprogram_npm/miniprogram-licia/random');
+const app = getApp();
 
 Page({
 
@@ -12,7 +13,10 @@ Page({
     currentTime: dateFormat(new Date(), 'yyyy年mm月dd日 HH:MM:ss'),
     minPrize: '0.01',
     maxPrize: '100.00',
-    msg: '点了就快去睡觉嗷！'
+    msg: '点了就快去睡觉嗷！',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   tapDialogButton(e) {
@@ -21,20 +25,67 @@ Page({
     })
   },
   tapSleepButton(e) {
-    var now = new Date();
-    var sleepTime = dateFormat(now, 'HH:MM:ss')
-    var prize = random(parseFloat(this.data.minPrize), parseFloat(this.data.maxPrize), true).toFixed(2);
+    const now = new Date();
+    const sleepTime = dateFormat(sleepTime, 'HH:MM:ss')
+    const prize = random(parseFloat(this.data.minPrize), parseFloat(this.data.maxPrize), true).toFixed(2);
+    if (this.data.maxPrize <= 0) {
+      // return;
+    }
     this.setData({
       dialogShow: true,
       sleepTime: sleepTime,
       prize: prize
-    })
+    });
+    this.saveSleep(now, prize);
+  },
+
+  saveSleep: function (sleepTime, prize) {
+    const date = dateFormat(sleepTime, 'yyyy-mm-dd');
+    const db = wx.cloud.database();
+    const sleep = db.collection('sleep');
+    const userInfo = this.data.userInfo;
+    sleep.add({
+      data: {
+        sleepTime: sleepTime,
+        prize: prize,
+        date: date,
+        userInfo: userInfo
+      }
+    }).then(res => {
+      console.log(res);
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
     this.loadInit();
     setInterval(() => {
       this.loadInit();
@@ -70,6 +121,16 @@ Page({
       minPrize = 0.01;
       maxPrize = 30.00;
     }
+
+    var nickName = this.data.userInfo.nickName;
+    if (nickName && nickName != '丫头' && nickName.indexOf('葱') < 0) {
+      minPrize = 0.00;
+      maxPrize = 0.00;
+      this.setData({
+        msg: '你不是橙宝宝！你是谁！'
+      });
+    }
+
     this.setData({
       currentTime: dateFormat(now, 'yyyy年mm月dd日 HH:MM:ss'),
       minPrize: minPrize.toFixed(2),
